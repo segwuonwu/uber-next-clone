e70df296cb394ddf474ee99206e52a3e06e4a68b
segwuonwu <segwuonwu@gmail.com>
2021-11-06 21:13:26 -0700

    Adding login and signout functionality

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import tw from "tailwind-styled-components";
 import Map from './components/map';
 import Link from 'next/link';
+import { auth } from '../firebase';
+import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    return onAuthStateChanged(auth, user => {
+      if (user) {
+        setUser({
+          name: user.displayName,
+          photoUrl: user.photoURL,
+        })
+      } else {
+        setUser(null)
+        router.push('/Login')
+      }
+    })
+  })
 
   return (
     <Wrapper>
@@ -14,8 +34,11 @@ export default function Home() {
         <Header>
           <UberLogo src="https://i.ibb.co/84stgjq/uber-technologies-new-20218114.jpg" />
           <Profile>
-            <Name>Solomon Eze</Name>
-            <UserImage src="https://assets.leetcode.com/users/segwuonwu/avatar_1579236613.png" />
+            <Name>{ user && user.name}</Name>
+            <UserImage
+              src={user && user.photoUrl}
+              onClick={() => signOut(auth)}
+            />
           </Profile>
         </Header>
 
@@ -67,7 +90,7 @@ const Name = tw.div`
 `
 
 const UserImage = tw.img`
-  h-12 w-12 rounded-full border-gray-200 p-px
+  h-12 w-12 rounded-full border-gray-200 p-px cursor-pointer
 `
 
 const ActionButtons = tw.div`
